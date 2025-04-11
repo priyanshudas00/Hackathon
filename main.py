a343b40e55c8ac483651e953c849675a02f20657
Priyanshu Raj <116489142+priyanshudas00@users.noreply.github.com>
2025-04-12 04:02:36 +0530

    main.py

--- main.py
+++ main.py
@@ -214,5 +214,6 @@ def blog():
 def upload():
     return render_template("upload.html")
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # Render sets the PORT env variable
+    app.run(host="0.0.0.0", port=port)
